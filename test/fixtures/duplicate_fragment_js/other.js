9a1f7759be11ff4d0deaa55f98e81d54f9db1391
Nathan Schwartz <nathan.schwartz95@gmail.com>
2021-04-29 11:50:59 -0500

    add support for tsx and jsx extensions

---
+++ test/fixtures/duplicate_fragment_js/other.js
@@ -0,0 +1,18 @@
+
+const frag = gql`
+  fragment jsdetails on Author {
+    firstName
+    lastName
+  }
+`;
+
+const query = gql`
+  query {
+    author {
+      ...jsdetails
+    }
+  }
+
+  ${frag}
+`;
+
--- test/fixtures/duplicate_fragment_ts/other.ts
+++ test/fixtures/duplicate_fragment_ts/other.ts
@@ -1,20 +1,3 @@
-const frag = gql`
-  fragment tsdetails on Author {
-    firstName
-    lastName
-  }
-`;
-
-const query = gql`
-  query {
-    author {
-      ...tsdetails
-    }
-  }
-
-  ${frag}
-`;
-
 const frag1 = gql`
   fragment tsdetails on Author {
     firstName
